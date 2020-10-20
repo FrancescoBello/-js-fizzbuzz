56d13c0e33d17f735eedb2d099c957e59c914f9e
Francesco <fraancesco.bello@gmail.com>
2020-10-20 17:29:58 +0200

    end

--- main.js
+++ main.js
@@ -5,15 +5,15 @@
 
 var numeri = 1;
 for (var i = 1; i <= 100; i = i + 1) {
-    console.log(i);
-    if (i % 3 == 0) {
+
+    if (i % 15 == 0) {
+    console.log("fizzbuzz");
+    }else if (i % 3 == 0) {
       console.log("fizz");
-    }
-    if (i % 5 == 0) {
+    } else if (i % 5 == 0) {
       console.log("buzz");
+    }else {
+      console.log(i);
     }
-  //  if (i % 3 && 5 == 0) {
-    //  console.log("fizzbuzz");
-    //}
 
 }
