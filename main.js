034464cade971e8253dbc6facea51c7781554762
Francesco <fraancesco.bello@gmail.com>
2020-10-20 17:07:11 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+var numeri = 1;
+for (var i = 1; i <= 100; i = i + 1) {
+    console.log(i);
+    if (i % 3 == 0) {
+      console.log("fizz");
+    }
+    if (i % 5 == 0) {
+      console.log("buzz");
+    }
+  //  if (i % 3 && 5 == 0) {
+    //  console.log("fizzbuzz");
+    //}
+
+}
